c6b22bd00c41bde991d8775edf22e7246d03403a
iftach <iftachshalev30115@gmail.com>
2021-02-06 21:08:03 +0200

    add server and client

--- try.py
+++ try.py
@@ -1,11 +1,10 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
-import wave
 
-fs = 44100  # Sample rate
-seconds = 3  # Duration of recording
+fs = 44100
+seconds = 3
 
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-sd.wait()  # Wait until recording is finished
-write('output.wav', fs, myrecording)  # Save as WAV file
-w = wave.open("output.wav", "rb")
+sd.wait()
+write('output.wav', fs, myrecording)
+print(bytes(myrecording))
