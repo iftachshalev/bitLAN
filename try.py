71548f9a6bfcd29fc3b1a97fb6200bebb3f9fa39
iftach <iftachshalev30115@gmail.com>
2021-02-07 13:26:56 +0200

    sound and record

--- try.py
+++ try.py
@@ -1,29 +1,12 @@
 import sounddevice as sd
-from scipy.io.wavfile import write
-from playsound import playsound
-from threading import Thread
-import time
 
 
-def play_sound():
-    playsound('output.wav')
+duration = 10
 
 
-def play_sound2():
-    playsound('output2.wav')
+def callback(indata, outdata, frames, time, status):
+    outdata[:] = indata
 
 
-fs = 44100
-seconds = 3
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-sd.wait()
-write('output.wav', fs, myrecording)
-thread = Thread(target=play_sound)
-thread.start()
-print("f")
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-sd.wait()
-write('output2.wav', fs, myrecording)
-thread = Thread(target=play_sound2)
-thread.start()
-print("f")
+with sd.Stream(channels=2, callback=callback):
+    sd.sleep(int(duration * 1000))
