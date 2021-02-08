280a43403a898b2efa97df6183bfd7e162231561
iftach <iftachshalev30115@gmail.com>
2021-02-08 13:09:25 +0200

    fix try.py

--- try.py
+++ try.py
@@ -1,17 +1,11 @@
 import sounddevice as sd
-from time import sleep
+duration = 5.5  # seconds
 
-fs = 8000# 16000, 44100
-block_size = 800
-duration = 10
-delay = 1
-sampl_arr = [0]*(fs*delay+block_size)
 
 def callback(indata, outdata, frames, time, status):
-    sampl_arr.append(indata)
-    outdata[:] = sampl_arr[-1*(fs*delay):-1*(fs*delay)+block_size]
-    print(frames, time, status)
+    print(indata)
+    outdata[:] = indata
 
 
-with sd.Stream(channels=1, callback=callback, samplerate=fs, blocksize=block_size, ):
+with sd.Stream(channels=1, callback=callback):
     sd.sleep(int(duration * 1000))
