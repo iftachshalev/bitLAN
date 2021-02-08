f6e80a3e875801437a27db7c22d8a35a374bd408
iftach <iftachshalev30115@gmail.com>
2021-02-08 18:38:34 +0200

    make double connection

--- client.py
+++ client.py
@@ -2,18 +2,36 @@ import sys, os
 from queue import Queue
 import time
 from communication import Communication
+import sounddevice as sd
+import numpy as np
+duration = 30
+
+
+def input_callback(indata, frames, time, status):
+    from_comp_buff.put(indata.tobytes())
+    print(from_comp_buff.qsize())
+
+
+def output_callback(outdata, frames, time, status):
+    print(from_sock_buff.qsize())
+    outdata[:, 0] = np.frombuffer(from_sock_buff.get(), dtype=np.int32)
+
 
 sys.path.append(os.path.abspath('.'))
 
 from_sock_buff = Queue()
 from_comp_buff = Queue()
 port = 12345
-frame_size = 32
+frame_size = 4096
+block_size = int(frame_size / 4)
 
 com = Communication(from_sock_buff, from_comp_buff, port, frame_size)
-com.connect('192.168.1.206')
+com.connect('192.168.1.153')
 
-for i in range(100):
-    from_comp_buff.put('hello from client: {}'.format(i).encode())
-    print(from_sock_buff.get())
-    time.sleep(0.2)
+with sd.InputStream(channels=1, callback=input_callback, dtype='int32', blocksize=block_size, samplerate=8000):
+    sd.sleep(int(duration * 1000))
+
+with sd.OutputStream(channels=1, callback=output_callback, dtype='int32',  blocksize=block_size, samplerate=8000):
+    sd.sleep(int(duration * 1000))
+
+com.close()
--- server.py
+++ server.py
@@ -1,19 +1,36 @@
-import sys, os
+import sys
+import os
 from queue import Queue
-
-sys.path.append(os.path.abspath('.'))
+import numpy as np
+import sounddevice as sd
 from communication import Communication
+duration = 30  # seconds
+sys.path.append(os.path.abspath('.'))
+
+
+def output_callback(outdata, frames, time, status):
+    print(from_sock_buff.qsize())
+    outdata[:, 0] = np.frombuffer(from_sock_buff.get(), dtype=np.int32)
+
+
+def input_callback(indata, frames, time, status):
+    from_comp_buff.put(indata.tobytes())
+    print(from_comp_buff.qsize())
+
 
 from_sock_buff = Queue()
 from_comp_buff = Queue()
 port = 12345
-frame_size = 32
+frame_size = 4096
+block_size = int(frame_size / 4)
 
 com = Communication(from_sock_buff, from_comp_buff, port, frame_size)
 com.listen()
 
-for i in range(100):
-    from_comp_buff.put('hello from server: {}'.format(i).encode())
-    print(from_sock_buff.get())
+with sd.OutputStream(channels=1, callback=output_callback, dtype='int32',  blocksize=block_size, samplerate=8000):
+    sd.sleep(int(duration * 1000))
+
+with sd.InputStream(channels=1, callback=input_callback, dtype='int32', blocksize=block_size, samplerate=8000):
+    sd.sleep(int(duration * 1000))
 
 com.close()
