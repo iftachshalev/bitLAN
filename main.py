3758b0717c09f272871dc750abe2582d23bb21da
Nadav Shalev <nadavshalev96@gmail.com>
2021-02-11 17:04:38 +0200

    fix read prob

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ from communication import Communication
 import sounddevice as sd
 import numpy as np
 import json
-duration = 30
 
 
 def output_callback(outdata, frames, time, status):
-	if conf['debug']:
-		print('out:\t{}\t{}'.format(from_sock_buff.qsize(), status))
-	data = from_sock_buff.get()
-	outdata[:, 0] = np.frombuffer(data, dtype=np.int32)
+    if conf['debug']:
+        print('out:\t{}\t{}'.format(from_sock_buff.qsize(), status))
+    data = from_sock_buff.get()
+    outdata[:, 0] = np.frombuffer(data, dtype=conf['data_type'])
 
 
 def input_callback(indata, frames, time, status):
-	if conf['debug']:
-		print('in:\t{}\t{}'.format(from_comp_buff.qsize(), status))
-	from_comp_buff.put(indata.tobytes())
+    if conf['debug']:
+        print('in:\t{}\t{}'.format(from_comp_buff.qsize(), status))
+    from_comp_buff.put(indata.tobytes())
 
 
 # read config file
@@ -29,9 +28,9 @@ with open('config.json') as json_file:
 from_sock_buff = Queue()
 from_comp_buff = Queue()
 
-data = b'\x00' * (self.frame_size)
+data = b'\x00' * (conf['frame_size'])
 for i in range(2):
-	from_sock_buff.put()
+    from_sock_buff.put(data)
 
 # init communication
 com = Communication(from_sock_buff, from_comp_buff, conf['port'], conf['frame_size'])
@@ -41,33 +40,33 @@ my_ip = Communication.get_ip()
 print('My ID is: ' + my_ip[my_ip.rfind('.') + 1:])
 connect_id = input('Enter ID (or blank if server)  >> ')
 if connect_id == '':
-	print('Wait for connections...')
-	com.listen()
-	print('Connected by: ' + repr(com.addr))
+    print('Wait for connections...')
+    com.listen()
+    print('Connected by: ' + repr(com.addr))
 else:
-	print('Connect to: ' + connect_id)
-	server_ip = my_ip[:my_ip.rfind('.')] + '.' + connect_id
-	com.connect(server_ip)
-	print('Connected!')
+    print('Connect to: ' + connect_id)
+    server_ip = my_ip[:my_ip.rfind('.')] + '.' + connect_id
+    com.connect(server_ip)
+    print('Connected!')
 
 # init block size
 block_size = int(conf['frame_size'] / conf['bytes_per_sample'])
 
-s_out = sd.OutputStream(channels=1, /
-						callback=output_callback, /
-						dtype=conf['data_type'],  /
-						blocksize=block_size, /
-						samplerate=conf['fs'])
+s_out = sd.OutputStream(channels=1,
+                        callback=output_callback,
+                        dtype=conf['data_type'],
+                        blocksize=block_size,
+                        samplerate=conf['fs'])
 
-s_in = sd.InputStream(channels=1, /
-						callback=input_callback, /
-						dtype=conf['data_type'], /
-						blocksize=block_size, /
-						samplerate=conf['fs'])
+s_in = sd.InputStream(channels=1,
+                      callback=input_callback,
+                      dtype=conf['data_type'],
+                      blocksize=block_size,
+                      samplerate=conf['fs'])
 
 # run
 with s_out, s_in:
-    sd.sleep(int(duration * 1000))
+    sd.sleep(int(conf['duration'] * 1000))
 
-close communication
+# close communication
 com.close()
