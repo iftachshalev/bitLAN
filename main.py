c007ca82525ad2739acd6e5e465043619c7a5bbc
Nadav Shalev <nadavshalev96@gmail.com>
2021-02-11 17:25:05 +0200

    add self loop test option

--- main.py
+++ main.py
@@ -5,20 +5,27 @@ from communication import Communication
 import sounddevice as sd
 import numpy as np
 import json
+from asyncio import QueueEmpty
+
+CHANNELS = 1  # support only 1
+SELF_LOOP_FLAG = True
 
 
-def output_callback(outdata, frames, time, status):
+def callback(indata, outdata, frames, time, status):
     if conf['debug']:
-        print('out:\t{}\t{}'.format(from_sock_buff.qsize(), status))
-    data = from_sock_buff.get()
-    outdata[:, 0] = np.frombuffer(data, dtype=conf['data_type'])
+        print('mic => {}\tspk => {}\t({})'.format(from_comp_buff.qsize(), from_sock_buff.qsize(), status))
 
+    # try get frame from queue. zeros if empty
+    try:
+        output_data_bytes = from_sock_buff.get_nowait()
+    except:
+        output_data_bytes = b'\x00' * conf['frame_size']
 
-def input_callback(indata, frames, time, status):
-    if conf['debug']:
-        print('in:\t{}\t{}'.format(from_comp_buff.qsize(), status))
+    # send data to speakers
+    outdata[:, 0] = np.frombuffer(output_data_bytes, dtype=conf['data_type'])
+
+    # get data from mic
     from_comp_buff.put(indata.tobytes())
-
 
 # read config file
 with open('config.json') as json_file:
@@ -27,10 +34,6 @@ with open('config.json') as json_file:
 # init queue
 from_sock_buff = Queue()
 from_comp_buff = Queue()
-
-data = b'\x00' * (conf['frame_size'])
-for i in range(2):
-    from_sock_buff.put(data)
 
 # init communication
 com = Communication(from_sock_buff, from_comp_buff, conf['port'], conf['frame_size'])
@@ -43,6 +46,12 @@ if connect_id == '':
     print('Wait for connections...')
     com.listen()
     print('Connected by: ' + repr(com.addr))
+
+    # For Test
+    # Server pass all data to client as is
+    if SELF_LOOP_FLAG:
+        while True:
+            from_comp_buff.put(from_sock_buff.get())
 else:
     print('Connect to: ' + connect_id)
     server_ip = my_ip[:my_ip.rfind('.')] + '.' + connect_id
@@ -52,20 +61,13 @@ else:
 # init block size
 block_size = int(conf['frame_size'] / conf['bytes_per_sample'])
 
-s_out = sd.OutputStream(channels=1,
-                        callback=output_callback,
-                        dtype=conf['data_type'],
-                        blocksize=block_size,
-                        samplerate=conf['fs'])
-
-s_in = sd.InputStream(channels=1,
-                      callback=input_callback,
-                      dtype=conf['data_type'],
-                      blocksize=block_size,
-                      samplerate=conf['fs'])
-
+s = sd.Stream(channels=CHANNELS,
+              callback=callback,
+              dtype=conf['data_type'],
+              blocksize=block_size,
+              samplerate=conf['fs'])
 # run
-with s_out, s_in:
+with s:
     sd.sleep(int(conf['duration'] * 1000))
 
 # close communication
